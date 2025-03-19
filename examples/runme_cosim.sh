95cf85367e031e9998c937d4761314af1b2a2f7e
Mark Pettigrew <mark.pettigrew@amd.com>
2025-03-19 16:23:08 -0700

    fix: better version detect

--- examples/runme_cosim.sh
+++ examples/runme_cosim.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export BLOCK=$1 ; rm -rf ${BLOCK}.comp/ ${BLOCK}.tcl ; COSIM="--cosim" make ${BLOCK}.comp
+export BLOCK=$1 ; rm -rf ${BLOCK}.comp/ ${BLOCK}.tcl ; COSIM="--cosim" CSYNTH="--csynth" make ${BLOCK}.comp
