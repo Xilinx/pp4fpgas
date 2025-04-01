b62b28de4054ab7e7bed807b0ba03ef31bc2846c
Mark Pettigrew <mark.pettigrew@amd.com>
2025-04-01 12:44:08 -0700

    fix(runme_cosim): add CSIM to runme_cosim.sh flow

--- examples/runme_cosim.sh
+++ examples/runme_cosim.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export BLOCK=$1 ; rm -rf ${BLOCK}.comp/ ${BLOCK}.tcl ; COSIM="--cosim" CSYNTH="--csynth" make ${BLOCK}.comp
+export BLOCK=$1 ; rm -rf ${BLOCK}.comp/ ${BLOCK}.tcl ; CSIM="--csim" COSIM="--cosim" CSYNTH="--csynth" make ${BLOCK}.comp
