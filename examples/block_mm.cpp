1a3e740d921e4c01c53cc4cfc008b4e954d0465b
Mark Pettigrew <mark.pettigrew@amd.com>
2025-04-02 12:31:00 -0700

    fix(block_mm): static A var needs STREAM pragma
    Use STREAM pragma (either type pipo or shared)

--- examples/block_mm.cpp
+++ examples/block_mm.cpp
@@ -1,14 +1,17 @@
 #include "block_mm.h"
+//#include "hls_print.h"
+
 void blockmatmul(hls::stream<blockvec> &Arows, hls::stream<blockvec> &Bcols,
         blockmat &ABpartial, int it) {
   #pragma HLS DATAFLOW
   int counter = it % (SIZE/BLOCK_SIZE);
-  static DTYPE A[BLOCK_SIZE][SIZE];
+  static DTYPE A[BLOCK_SIZE][SIZE] = { 0 };
+  #pragma HLS STREAM variable=A type=shared depth=1
   if(counter == 0){ //only load the A rows when necessary
     loadA_i: for(int i = 0; i < SIZE; i++) {
       blockvec tempA = Arows.read();
       loadA_j: for(int j = 0; j < BLOCK_SIZE; j++) {
-//        #pragma HLS PIPELINE II=1
+        #pragma HLS PIPELINE II=1
         A[j][i] = tempA.a[j];
       }
     }
