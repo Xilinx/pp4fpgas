7883d05d9346ccbf6b7d9de87373d97ac7505612
Mark Pettigrew <mark.pettigrew@amd.com>
2025-04-25 12:02:37 -0700

    fix: add vitis-run arg support
    Add support to pass
    arguments to vitis-run
    CLI.

--- examples/runme_csim.sh
+++ examples/runme_csim.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-export BLOCK=$1 ; rm -rf ${BLOCK}.comp/ ${BLOCK}.tcl ; CSIM="--csim" make ${BLOCK}.comp
+export BLOCK=$1
+rm -rf ${BLOCK}.comp/ ${BLOCK}.tcl
+VITIS_HLS_ARGS=${VITIS_HLS_ARGS:+$VITIS_HLS_ARGS}  CSIM="--csim" make ${BLOCK}.comp
