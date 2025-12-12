8fcb9474723874847ea8f5840f407764de2c8dad
Pettigrew, Mark <Mark.Pettigrew@amd.com>
2025-12-12 16:27:26 -0500

    fix(spmv2_restructured): DEPENDENCE pragma settings (#3)
    cr-1249411

--- examples/spmv2_restructured.cpp
+++ examples/spmv2_restructured.cpp
@@ -31,7 +31,7 @@ void spmv(int rowPtr[NUM_ROWS+1], int columnIndex[NNZ],
  ACC:
 	for(int i=0; i<NNZ * S + NUM_ROWS; i++ ) {
 		int j = i % S;
-#pragma HLS DEPENDENCE variable=y inter distance=8
+#pragma HLS DEPENDENCE variable=y inter distance=S direction=RAW
 #pragma HLS PIPELINE II=1
 
 		if(LB[j] == UB[j]) {
